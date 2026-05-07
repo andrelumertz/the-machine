cde20dc870da6ee4d42352d4d43b6954e973b32c
André Lumertz <andrelumertzmartins@gmail.com>
2026-05-07 15:58:28 -0300

    security: protege rotas administrativas com validação de sessão

--- admin.php
+++ admin.php
@@ -1,5 +1,11 @@
 <?php 
     
+    session_start();
+    if (!isset($_SESSION['usuario_id'])) {
+    header("Location: login.php");
+    exit();
+    }
+
     require "src/conexao-bd.php";
     require "src/Modelo/Produto.php";
     require "src/Modelo/Repositorio/produtoRepositorio.php";
